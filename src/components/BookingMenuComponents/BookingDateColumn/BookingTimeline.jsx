0a494c50ddf63ef2ec9895ad0457024ec65b46b9
Ortsukh <gex_92@mail.ru>
2023-08-14 10:08:24 +0300

    hot fix

--- src/components/BookingMenuComponents/BookingDateColumn/BookingTimeline.jsx
+++ src/components/BookingMenuComponents/BookingDateColumn/BookingTimeline.jsx
@@ -32,14 +32,9 @@ export const BookingTimeline = ({ selectedGroups, groups, items }) => {
   const [currentMonth, setCurrentMonth] = useState(today);
   const [showCalendar, setShowCalendar] = useState(false);
 
-<<<<<<< HEAD
   const [openGroups, setOpenGroups] = useState(false);
 
   const [currentDevice, setCurrentDevice] = useState("---");
-=======
-  const [currentDevice, setCurrentDevice] = useState([]);
-  // console.log(currentDevice);
->>>>>>> main
 
   const handleBoundsChange = (visibleTimeStart, visibleTimeEnd) => {
     setVisibleTimeStart(visibleTimeStart);
