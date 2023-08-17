61d21b5568cc06ad40ccf7fe7926b89e16c3ad45
Ortsukh <gex_92@mail.ru>
2023-08-17 11:47:52 +0300

    feat add order collection box

--- src/components/BookingMenuComponents/BookingMenu.jsx
+++ src/components/BookingMenuComponents/BookingMenu.jsx
@@ -1,5 +1,8 @@
 import moment from "moment";
 import { useState } from "react";
+import { createOrder, sendEditOrder } from "../../Api/API";
+import { createOrderGrid, formatOrder } from "../../DataConvertHelper";
+import ConfirmWindow from "../ConfirmWindow";
 import { BookingTimeline } from "./BookingDateColumn/BookingTimeline";
 import style from "./BookingMenu.module.css";
 import { EditButtonColumn } from "./EditButtonColumn/EditButtonColumn";
@@ -22,6 +25,7 @@ export const BookingMenu = ({
   setCurrentDevice,
   setIsEditMode,
   operAlertWindow,
+  allGroups,
   //! ToolsFilter->
   toolNames,
   onInputChange,
@@ -35,6 +39,8 @@ export const BookingMenu = ({
   const [itemsPreOrder, setItemsPreOrder] = useState([]);
   const [updatedItems, setUpdatedItems] = useState(items);
   const [copyEditItems, setCopyEditItems] = useState([]);
+  const [isConfirmWindowOpen, setIsConfirmWindowOpen] = useState(false);
+  const [orderContent, setOrderContent] = useState([]);
   const [orderDatePlanning, setOrderDatePlanning] = useState({
     selection1: {
       startDate: new Date(),
@@ -43,8 +49,59 @@ export const BookingMenu = ({
       key: "selection1",
     },
   });
-  const initialCurrentDeviceIndex = groups.map(current => current.id).indexOf(currentDevice.id)
-  const [currentDeviceIndex, setCurrentDeviceIndex] = useState(initialCurrentDeviceIndex);
+  const initialCurrentDeviceIndex = groups
+    .map((current) => current.id)
+    .indexOf(currentDevice.id);
+  const [currentDeviceIndex, setCurrentDeviceIndex] = useState(
+    initialCurrentDeviceIndex
+  );
+
+  const editOrder = () => {
+    if (itemsPreOrder.length < 1) return;
+    let itemsPreOrderCorrect = itemsPreOrder.map((item) => {
+      item.group = item.deviceGroup;
+      return item;
+    });
+    const orderItem = copyEditItems[0];
+    const orderItemsGrid = createOrderGrid(itemsPreOrderCorrect);
+    const dateIntervals = formatOrder(orderItemsGrid);
+    const editedOrder = {
+      rentOrder: {
+        id: orderItem.rentOrderId,
+        company: orderItem.company,
+      },
+      status: orderItem.status,
+      equipmentItems: dateIntervals,
+    };
+
+    sendEditOrder(editedOrder)
+      .then(() => {
+        operAlertWindow("success");
+        setUpdate((previousUpdate) => !previousUpdate);
+        setItemsPreOrder([]);
+        setCopyEditItems([]);
+      })
+      .catch(() => operAlertWindow("error"));
+  };
+
+  const sendNewOrder = () => {
+    if (itemsPreOrder.length < 1) return;
+    let itemsPreOrderCorrect = itemsPreOrder.map((item) => {
+      item.group = item.deviceGroup;
+      return item;
+    });
+    const orderItems = createOrderGrid(itemsPreOrderCorrect);
+    createOrder(orderItems)
+      .then((response) => {
+        operAlertWindow("success");
+        setItemsPreOrder([]);
+        setIsBookingMenu(false);
+        setCurrentDevice([]);
+        setShowButtonClear(true);
+        setUpdate((previousUpdate) => !previousUpdate);
+      })
+      .catch(operAlertWindow("error"));
+  };
 
   console.log("updatedItems", updatedItems, itemsPreOrder);
   return (
@@ -52,7 +109,6 @@ export const BookingMenu = ({
       <div className={style.container}>
         <div className={style.editButtonColumn}>
           <EditButtonColumn
-            setUpdate={setUpdate}
             setIsBookingMenu={setIsBookingMenu}
             itemsPreOrder={itemsPreOrder}
             setItemsPreOrder={setItemsPreOrder}
@@ -68,6 +124,9 @@ export const BookingMenu = ({
             orderDate={orderDatePlanning}
             setOrderDate={setOrderDatePlanning}
             setCurrentDeviceIndex={setCurrentDeviceIndex}
+            groups={allGroups}
+            setIsConfirmWindowOpen={setIsConfirmWindowOpen}
+            setOrderContent={setOrderContent}
             //! ToolsFilter->
             toolNames={toolNames}
             onInputChange={onInputChange}
@@ -92,7 +151,7 @@ export const BookingMenu = ({
             itemsPreOrder={itemsPreOrder}
             setItemsPreOrder={setItemsPreOrder}
             setUpdatedItems={setUpdatedItems}
-            setCopyEditItems ={setCopyEditItems }
+            setCopyEditItems={setCopyEditItems}
             toolsCount={toolsCount}
             isActiveDate={isActiveDate}
             orderDate={orderDate}
@@ -106,6 +165,13 @@ export const BookingMenu = ({
           />
         </div>
       </div>
+      {isConfirmWindowOpen  && (
+        <ConfirmWindow
+          data={orderContent}
+          closeBookingWindow={setIsConfirmWindowOpen}
+          confirmFunc={isEditMode ? editOrder : sendNewOrder}
+        />
+      )}
     </>
   );
 };
--- src/components/MessageWindow.js
+++ src/components/MessageWindow.js
@@ -1,21 +1,12 @@
 import React from "react";
 import "./style.css";
 
-export default function MessageWindow({ data, closeBookingWindow, editMode }) {
+export default function EditWindow({ data, closeBookingWindow, editMode }) {
   const heightModal = 185;
   const weightModal = 225;
 
   const isLeftPart = window.innerWidth / 2 - data.posX > 0;
-  const modalConstants = {
-    clickOnEmpty: {
-      companyName: null,
-      buttonName: "Забранировать",
-    },
-    clickOnOrder: {
-      companyName: "Название компании:",
-      buttonName: null,
-    },
-  };
+
 
   return (
     <div
@@ -29,26 +20,20 @@ export default function MessageWindow({ data, closeBookingWindow, editMode }) {
         x
       </button>
       <div className="messageWindow-item">
-        <span>Cмена:</span>
+        <span>{"Смена"}:</span>
         <span>{data.date}</span>
       </div>
 
-      {modalConstants[data.kindModal].companyName ? (
-        <div className="messageWindow-item">
-          <span>{modalConstants[data.kindModal].companyName}</span>
-          <span>{data.item.company.name}</span>
-        </div>
-      ) : null}
-
-
-        <div className="messageWindow-item">
+      <div className="messageWindow-item">
         <span>{"Статус:"}</span>
         <span>{data.item.status}</span>
       </div>
-        <button className="button-submit reserved-btn" onClick={(e)=> editMode(e, data.item)}>Редактировать</button>
- 
-        
-      
+      <button
+        className="button-submit reserved-btn"
+        onClick={(e) => editMode(e, data.item)}
+      >
+        {"Редактировать"}
+      </button>
     </div>
   );
 }
