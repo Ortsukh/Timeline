0db27370daba6ac3d420ec39d06077a17151e6dc
Ortsukh <gex_92@mail.ru>
2023-09-22 11:48:47 +0300

    add tooltip

--- src/common/GenerateElementsData.jsx
+++ src/common/GenerateElementsData.jsx
@@ -1,5 +1,27 @@
 import moment from "moment";
 import React from "react";
+import ITEMS_PREORDER_COLOR from "../constants/itemsPreOrderColor";
+
+export const generateClue = () => {
+  const clueContent = [];
+  Object.keys(ITEMS_PREORDER_COLOR).forEach((status) => {
+    clueContent.push(
+      <div className="clueItem">
+        <div
+          className="clueItemColor"
+          style={{
+            backgroundColor: ITEMS_PREORDER_COLOR[status].backgroundColor,
+          }}
+        />
+        <span style={{ maxWidth: 250 }}>
+          {" "}
+          {ITEMS_PREORDER_COLOR[status].text}
+        </span>
+      </div>,
+    );
+  });
+  return clueContent;
+};
 
 export default function generateCheckBox(
   selectedDateSampleGrid,
