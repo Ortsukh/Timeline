682cbaf835d5750a68710154ad3b074ecd7753d5
mikitsch <nikishby@gmail.com>
2023-09-28 13:05:16 +0300

    fix: PNG image of Sasha

--- src/others/svgImport.js
+++
@@ -1,9 +0,0 @@
-import ConflSVG from "./svg/confl.svg";
-import ConflBSVG from "./svg/conflb.svg";
-import RedCircleCross from "./redCircleCross.png";
-
-export {
-  ConflSVG,
-  ConflBSVG,
-  RedCircleCross,
-};
