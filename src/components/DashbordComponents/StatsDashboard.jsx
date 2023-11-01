096214c4ac3743d25d8f130aa50db4656c0cd032
Ortsukh <gex_92@mail.ru>
2023-11-01 15:58:47 +0300

    add stats

---
+++ src/components/DashbordComponents/StatsDashboard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+export default function StatsDashboard() {
+  return (
+    <div className="statsDashboardContainer">
+      <div className="statsItem">
+        <span>Прибыль за день</span>
+        <span>2600</span>
+      </div>
+      <div className="statsItem">
+        <span>Прибыль за период</span>
+        <span>23300</span>
+      </div>
+      <div className="statsItem">
+        <span>Просроченные платежи</span>
+        <span>0</span>
+      </div>
+      <div className="statsItem">
+        <span>Количество транзакций</span>
+        <span>200</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/ManagerDashboard.jsx
+++ src/pages/ManagerDashboard.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
-import moment from "moment";
+import React from "react";
 import UserBox from "../components/DashbordComponents/UserBox";
 import ProfitByTimeChart from "../components/DashbordComponents/ProfitByTimeChart";
 import RenterTableDashboard from "../components/DashbordComponents/RenterTableDashboard";
-import UserAccountDashBoard from "../components/DashbordComponents/UserAccountDashBoard";
 import RepairKitchenTableDashboard from "../components/DashbordComponents/RepairKitchenTableDashboard";
 import LastOrdersTableDashboard from "../components/DashbordComponents/LastOrdersTableDashboard";
-import CalendarDashboard from "../components/DashbordComponents/CalendarDashboard";
+import StatsDashboard from "../components/DashbordComponents/StatsDashboard";
 
 export default function ManagerDashboard() {
   console.log("load");
@@ -16,8 +14,11 @@ export default function ManagerDashboard() {
         <div className="col-lg-3 col-md-4">
           <UserBox />
         </div>
-
-        <div className="col-lg-6 col-md-8"><ProfitByTimeChart /></div>
+        <div className="col-lg-6 col-md-8">
+          <ProfitByTimeChart />
+          {" "}
+          <StatsDashboard />
+        </div>
 
       </div>
       <div className="row">
