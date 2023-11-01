f9e42c73fb5689ba3a91d8900776ce3db97b57fc
mikitsch <nikishby@gmail.com>
2023-11-01 12:30:01 +0300

    fix: changed the name of statuses when clicking on an item in the main Timeline

--- src/components/Popup/MessageWindow.jsx
+++ src/components/Popup/MessageWindow.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../style.css";
-import orderStatus from "../../constants/constants";
+import moment from "moment";
+// import orderStatus from "../../constants/constants";
 import buttonTitleConstants from "../../constants/buttonTitleConstants";
 
 export default function MessageWindow({
@@ -18,6 +19,16 @@ export default function MessageWindow({
   const weightModal = 225;
   const isLeftPart = window.innerWidth / 2 - data.posX > 0;
   const isBottomPart = window.innerHeight / 2 - data.posY > 0;
+
+  const statusDisplay = (date, status) => {
+    if (status === "pending") return "Новый";
+    if (status === "accepted") {
+      const completionDate = date.replace(/\/ \d{2}:\d{2}-/, "");
+      const isCompleted = moment(completionDate, "YYYY-MM-DD HH:mm").isBefore(moment());
+      return isCompleted ? "Завершён" : "В работе";
+    }
+    return "Отклонён";
+  };
 
   return (
     <div
@@ -39,7 +50,8 @@ export default function MessageWindow({
       </div>
       <div className="messageWindow-item status-messageWindow">
         <span>Статус:</span>
-        <span>{orderStatus[data.item.status]?.translatRU}</span>
+        {/* <span>{orderStatus[data.item.status]?.translatRU}</span> */}
+        <span>{statusDisplay(data.date, data.item.status)}</span>
       </div>
       {comment
         && (
@@ -55,10 +67,10 @@ export default function MessageWindow({
 
       {data.item.status === "accepted"
         && (
-          <div className="messageWindow-item">
-            <span>Компания:</span>
-            <span>{data.item.company.name}</span>
-          </div>
+        <div className="messageWindow-item">
+          <span>Компания:</span>
+          <span>{data.item.company?.name}</span>
+        </div>
         )}
       {data.item.status === "pending"
         && (
