3ab8dd0cd2274176ad0c8052c437a7a9512e64d0
AleksaGrri <aleksgrishenckova@yandex.com>
2023-11-02 14:01:52 +0300

    stst

--- src/components/DashbordComponents/ManagerStatsDashboard.jsx
+++ src/components/DashbordComponents/ManagerStatsDashboard.jsx
@@ -4,21 +4,21 @@ import "./style.css";
 export default function ManagerStatsDashboard() {
   return (
     <div className="statsDashboardContainer">
-      <div className="statsItem">
+      <div className="statsItem item-green-bg">
         <span>Прибыль за день</span>
         <span className="positive-values values">2600</span>
       </div>
-      <div className="statsItem">
+      <div className="statsItem item-green-bg">
         <span>Прибыль за период</span>
         <span className="positive-values values">
           {Math.floor(Math.random() * 30000 + 15000)}
         </span>
       </div>
-      <div className="statsItem">
+      <div className="statsItem item-red-bg">
         <span>Просроченные платежи</span>
         <span className="negative-values values">0</span>
       </div>
-      <div className="statsItem">
+      <div className="statsItem item-green-bg">
         <span>Количество транзакций</span>
         <span className="positive-values values">200</span>
       </div>
