bcf0c4f7f4591dded24ad40af5290132dd468bd9
Ortsukh <gex_92@mail.ru>
2023-11-03 11:15:30 +0300

    add fild into stats

--- src/components/DashbordComponents/LesseeStatsDashboard.jsx
+++ src/components/DashbordComponents/LesseeStatsDashboard.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./style.css";
 
-export default function LesseeStatsDashboard() {
+export default function LesseeStatsDashboard({ profitItems }) {
+  const calcAllProfit = () => {
+    let result = 0;
+    result = profitItems.reduce((acc, cur) => cur.y + acc, 0);
+    return result;
+  };
   return (
     <div className="statsDashboardContainer">
       <div className="statsItem">
         <span>Затраты за день</span>
-        <span className="positive-values values">2600</span>
+        <span className="positive-values values">
+          {profitItems[profitItems.length - 1].y.toFixed(2)}
+        </span>
       </div>
       <div className="statsItem">
         <span>Затраты за период</span>
         <span className="positive-values values">
-          {Math.floor(Math.random() * 30000 + 15000)}
+          {calcAllProfit().toFixed(2)}
         </span>
+      </div>
+      <div className="statsItem">
+        <span>Забронировано</span>
+        <span className="negative-values values">124.00</span>
       </div>
       <div className="statsItem">
         <span>Просроченные платежи</span>
